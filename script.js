e4a28cf260b875b94ff4c0af55b90bc9ce007b86
a-yousefi1 <a.yousefi4784@gmail.com>
2023-11-27 23:03:36 +0330

    header & color reset button added

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+/* generate random color code */
 function random_color() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
+    let letters = '0123456789ABCDEF';
+    let color = '#';
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
+/* change background color & color code in span tag */
 function click_to_change() {
     let element = document.getElementById("color_code");
-    var randomColor = random_color();
+    let randomColor = random_color();
     document.body.style.backgroundColor = randomColor;
     element.textContent = randomColor
+}
+
+/* reset color button */
+function reset_color(){
+    let color = "#ffff";
+    let element = document.getElementById("color_code");
+    document.body.style.backgroundColor = color
+    element.textContent = color
 }
